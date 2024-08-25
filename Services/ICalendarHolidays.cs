05b38c0a19b5aa02dd39f5699b11c226de1bb44b
Ousniba <745768840@qq.com>
2024-08-25 15:39:46 +0800

    主页、日历、Header、Footer

---
+++ Services/ICalendarHolidays.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBlazorApp.Services
+{
+    public interface ICalendarHolidays
+    {
+        /// <Summary>
+        /// 获得 假日键值
+        /// </Summary>
+        bool IsHoliday(DateTime dt);
+    }
+}
